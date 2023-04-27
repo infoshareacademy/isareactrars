e7f040623262df61299b4f2f2462cb1f064651ac
Kamil Richert <krichert@atlassian.com>
2023-04-27 14:10:05 +0200

    Game component as state example

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
 import { Wrapper } from './Wrapper';
 import { AboutMe } from './about-me/AboutMe';
+import { Game } from './game/Game';
+
 function App() {
   return (
     <Wrapper>
+        <Game name="Counter Strike" />
         <AboutMe />
     </Wrapper>
   );
