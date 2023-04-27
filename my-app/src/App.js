bd2d2f4f6941a1f61b82dee3bc608538e66fb396
Kamil Richert <krichert@atlassian.com>
2023-04-27 16:58:46 +0200

    added content component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
 import { Navigation } from './Navigation';
-import { Wrapper } from './Wrapper';
-import { AboutMe } from './about-me/AboutMe';
-import { Game } from './game/Game';
-import { Users } from './users/Users';
-import { MyForm } from './forms/MyForm';
-import { MyBetterForm } from './forms/MyBetterForm';
-import { MyBoostedForm } from './forms/MyBoostedForm';
+import { Content } from './Content';
 
 function App() {
   return (
     <>
         <Navigation />
-        <Wrapper>
-            <Routes>
-                <Route path="/" element={<h1>Witaj na mojej stronie!</h1>} />
-                <Route path="/about-me" element={<AboutMe />} />
-                <Route path="/game" element={<Game name="Counter Strike" />} />
-                <Route path="/users" element={<Users />} />
-                <Route path="/forms" element={<>
-                        <MyBoostedForm />
-                        <MyBetterForm />
-                        <MyForm />
-                    </>} />
-            </Routes>
-        </Wrapper>
+        <Content />
     </>
   );
 }
---
+++ my-app/src/Content.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import { AboutMe } from './about-me/AboutMe';
+import { Game } from './game/Game';
+import { Users } from './users/Users';
+import { MyForm } from './forms/MyForm';
+import { MyBetterForm } from './forms/MyBetterForm';
+import { MyBoostedForm } from './forms/MyBoostedForm';
+import { Wrapper } from './Wrapper';
+
+export const Content = () => (
+    <Wrapper>
+        <Routes>
+            <Route path="/" element={<h1>Witaj na mojej stronie!</h1>} />
+            <Route path="/about-me" element={<AboutMe />} />
+            <Route path="/game" element={<Game name="Counter Strike" />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/forms" element={<>
+                    <MyBoostedForm />
+                    <MyBetterForm />
+                    <MyForm />
+                </>} />
+        </Routes>
+    </Wrapper>  
+)
