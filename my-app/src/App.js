77d5ffe375c008774cdd1802cdda5ef1e84e3e6f
Kamil Richert <krichert@atlassian.com>
2023-04-27 12:13:37 +0200

    Added Wrapper and my favourite dishes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import { Contact } from './Contact';
+import { Wrapper } from './Wrapper';
+import { MyFavouriteDishes } from './MyFavouriteDishes';
 import MyName from './MyName';
 
 function App() {
   return (
-    <>
+    <Wrapper>
         <MyName name="Janusz" surname="Kowalski" />
         <MyName name="Janusz" />
         <Contact data={{
@@ -11,7 +13,13 @@ function App() {
             address: { street: 'Słowackiego', city: 'Gdańsk', number: 37 }, 
             email: 'moj@mail.com'
         }} />
-    </>
+        <MyFavouriteDishes dishes={[
+            { id: 1, name: 'spaghetti'},
+            { id: 2, name: 'burger'},
+            { id: 3, name: 'stek'},
+            { id: 4, name: 'gołąbki'},
+        ]} />
+    </Wrapper>
   );
 }
 
