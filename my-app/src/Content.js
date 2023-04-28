b67b7b3507eee019152ce7d94c47c686e8e2b514
Kamil Richert <krichert@atlassian.com>
2023-04-28 09:54:31 +0200

    Added code spliting

--- my-app/src/Content.js
+++ my-app/src/Content.js
@@ -1,6 +1,6 @@
+import {lazy, Suspense} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Sign } from './sign/Sign';
-import { AboutMe } from './about-me/AboutMe';
 import { Game } from './game/Game';
 import { Users } from './users/Users';
 import { MyForm } from './forms/MyForm';
@@ -8,21 +8,27 @@ import { MyBetterForm } from './forms/MyBetterForm';
 import { MyBoostedForm } from './forms/MyBoostedForm';
 import { Wrapper } from './Wrapper';
 import { UserDetails } from './users/UserDetails';
+import { CircularProgress } from '@mui/material';
+
+const LazyAboutMe = lazy(() => import('./about-me/AboutMe')
+    .then(module => ({ default: module.AboutMe})))
 
 export const Content = () => (
     <Wrapper>
-        <Routes>
-            <Route path="/" element={<h1>Witaj na mojej stronie!</h1>} />
-            <Route path="/about-me" element={<AboutMe />} />
-            <Route path="/game" element={<Game name="Counter Strike" />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/users/:id" element={<UserDetails />} />
-            <Route path="/forms" element={<>
-                    <MyBoostedForm />
-                    <MyBetterForm />
-                    <MyForm />
-                </>} />
-            <Route path="/sign" element={<Sign />} />
-        </Routes>
+        <Suspense fallback={<CircularProgress />}>
+            <Routes>
+                <Route path="/" element={<h1>Witaj na mojej stronie!</h1>} />
+                <Route path="/about-me" element={<LazyAboutMe />} />
+                <Route path="/game" element={<Game name="Counter Strike" />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/users/:id" element={<UserDetails />} />
+                <Route path="/forms" element={<>
+                        <MyBoostedForm />
+                        <MyBetterForm />
+                        <MyForm />
+                    </>} />
+                <Route path="/sign" element={<Sign />} />
+            </Routes>
+        </Suspense>
     </Wrapper>  
 )
