3617d140f894bdbcdf6abed85254b3115d07d5fc
Kamil Richert <krichert@atlassian.com>
2023-04-28 08:25:44 +0200

    Added navlink in navigation

--- my-app/src/Navigation.js
+++ my-app/src/Navigation.js
@@ -1,8 +1,18 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+
+const links = [
+    { label: 'Home', path: '/' },
+    { label: 'Game', path: '/game' },
+    { label: 'Users', path: '/users' },
+    { label: 'Forms', path: '/forms' },
+    { label: 'About me', path: '/about-me' },
+];
+
+const getActiveStyles = ({ isActive }) => ({ textDecoration: isActive ? 'underline' : undefined})
 
 export const Navigation = () => {
     return <AppBar position="static">
@@ -10,11 +20,11 @@ export const Navigation = () => {
         <Typography variant="h6" component="div" sx={{ marginRight: 3 }}>
           My App
         </Typography>
-        <Button color="inherit" component={Link} to="/">Home</Button>
-        <Button color="inherit" component={Link} to="/game">Game</Button>
-        <Button color="inherit" component={Link} to="/users">Users</Button>
-        <Button color="inherit" component={Link} to="/forms">Forms</Button>
-        <Button color="inherit" component={Link} to="/about-me">About me</Button>
+        {
+            links.map(link => (
+                <Button color="inherit" component={NavLink} style={getActiveStyles} to={link.path}>{link.label}</Button>
+            ))
+        }
       </Toolbar>
     </AppBar>
 }
