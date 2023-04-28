9d73374b58e28eb6b9806ec55d2232662ee71e99
Kamil Richert <krichert@atlassian.com>
2023-04-28 13:51:41 +0200

    Added fetching burgers

--- burgers-app/src/content/menu/index.tsx
+++ burgers-app/src/content/menu/index.tsx
@@ -6,8 +6,29 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useEffect, useState } from "react";
+
+type Burger = {
+    name: string,
+    price: string,
+    restaurant: number,
+    id: number,
+    ingredients: {
+        [key: string]: number
+    }
+}
 
 export const Menu = () => {
+    const [burgers, setBurgers] = useState<Burger[]>([]);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/burger')
+            .then(r => r.json())
+            .then(data => {
+                setBurgers(data);
+            })
+    }, []);
+
     return (
         <PageWrapper title="Menu">
             <TableContainer component={Paper}>
@@ -15,11 +36,20 @@ export const Menu = () => {
                     <TableHead>
                     <TableRow>
                         <TableCell>Name</TableCell>
+                        <TableCell align="right">Ingredients</TableCell>
                         <TableCell align="right">Price</TableCell>
                     </TableRow>
                     </TableHead>
                     <TableBody>
-                    
+                    {
+                        burgers.map(burger => (
+                            <TableRow key={burger.id}>
+                                <TableCell>{burger.name}</TableCell>
+                                <TableCell align="right">{Object.keys(burger.ingredients).join(', ')}</TableCell>
+                                <TableCell align="right">{burger.price}</TableCell>
+                            </TableRow>
+                        ))
+                    }
                     </TableBody>
                 </Table>
             </TableContainer>
